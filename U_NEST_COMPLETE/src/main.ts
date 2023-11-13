beb46cce4660c458d0f52e059f91a1965e1a0fcb
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2023-11-13 16:14:10 +0600

    nest js from scratch

---
+++ U_NEST_COMPLETE/src/main.ts
@@ -0,0 +1,28 @@
+import { Controller, Module, Get } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+
+@Controller()
+class AppController {
+  @Get()
+  index() {
+    return {
+      status: 200,
+      message: "this is home page of our application",
+    };
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.listen("4545", () => {
+    console.log(`Your server is running at port ${4545}...`);
+  });
+}
+
+bootstrap();
