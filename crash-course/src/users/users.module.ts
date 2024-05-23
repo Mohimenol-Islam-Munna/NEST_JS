d1c811c98bd29038db30bae0d4983cbfdef5398c
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-05-23 16:00:39 +0600

    remove extra file

--- crash-course/src/users/users.module.ts
+++ crash-course/src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './controller/user/user.controller';
 import { AdminController } from './controller/admin/admin.controller';
+import { UsersService } from './services/users/users.service';
 
 @Module({
-  controllers: [UserController, AdminController]
+  controllers: [UserController, AdminController],
+  providers: [UsersService]
 })
 export class UsersModule {}
