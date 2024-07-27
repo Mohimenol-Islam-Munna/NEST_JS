e103a1451c65b2f4576eceb99d1f27b935820bae
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-07-28 02:38:48 +0600

    nest js and prisma orm

--- july-2024/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import MessageModule from './message/message.module';
-
-const bootstrap = async () => {
-  const app = await NestFactory.create(MessageModule);
-  await app.listen(8080);
-};
-
-bootstrap();
