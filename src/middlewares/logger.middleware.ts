93d34c9da53eb635c5f1418ba5dd2874116c2201
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-07-28 17:36:28 +0600

    nest middleware

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,8 @@
+import { NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('nest js middleware (logger)');
+  }
+}
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductModule } from '../product/product.module';
+import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
 
 @Module({
   imports: [ProductModule],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware);
+  }
+}
