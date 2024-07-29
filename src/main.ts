01d9db436a9e906bb558823fd13f0381add3d36f
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-07-29 13:52:43 +0600

    global middleware

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import swaggerConfigHandler from './config/swagger/swagger.config';
+import { tracerMiddleware } from './middlewares/functional.middleware/tracer.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // swagger configuration
   swaggerConfigHandler(app);
+
+  // global middleware
+  app.use(tracerMiddleware);
 
   await app.listen(8080);
 }
